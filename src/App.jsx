4c29483f06dbe2c1d3d98c066017c8bb4bd7054e
Eten-Ekata <ekataelishaeten@gmail.com>
2023-07-14 20:25:13 +0100

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './index.css'
+import Logo from './components/Logo'
+import Form from './components/Form'
+import Item from './components/Item'
+import PackingList from './components/PackingList'
+import Stats from './components/Stats'
 
 function App() {
 
   return (
     <div className='app'>
-     
+     <Logo />
+     <Form />
+     <PackingList />
+     <Stats />
     </div>
   )
 }
