a4ced98d3a5508c2bcc444ec6134e8ab45b174ac
Eten-Ekata <ekataelishaeten@gmail.com>
2023-07-14 21:35:18 +0100

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Form = () => {
+    const [quantity, setQuantity] = useState(1);
+    const [description, setDescription] = useState("");
+
+    const handleSubmit=()=>{
+        e.preventDefault();  
+    }
   return (
-    <div>Form</div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   )
 }
 
