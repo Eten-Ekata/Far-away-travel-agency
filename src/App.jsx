8ae35db4f40ff43b50efbba2592b5885daf851ea
Eten-Ekata <ekataelishaeten@gmail.com>
2023-07-15 20:47:47 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
 import './index.css'
 import Logo from './components/Logo'
 import Form from './components/Form'
-import Item from './components/Item'
 import PackingList from './components/PackingList'
 import Stats from './components/Stats'
 
 function App() {
-  // const [items, setItems] = useState([]);
-
-  // const handleAddItems=(item) =>{
-  //   setItems((items) => [...items, item]);
-  // }
-
-  // const handleDeleteItem=(id)=>{
-  //   setItems((items) => items.filter((item) => item.id !== id));
-  // }
-
-  // const handleToggleItem=(id)=>{
-  //   setItems((items) =>
-  //     items.map((item) =>
-  //       item.id === id ? { ...item, packed: !item.packed } : item
-  //     )
-  //   );
-  // }
-
-  // const handleClearList=()=>{
-  //   const confirmed = window.confirm(
-  //     "Are you sure you want to delete all items?"
-  //   );
-
-  //   if (confirmed) setItems([]);
-  // }
-
+  
   return (
     <div className='app'>
      <Logo />
      <Form />
-     <PackingList  
-     />
+     <PackingList/>
      <Stats />
     </div>
   )
